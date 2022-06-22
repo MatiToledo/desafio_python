933daa8325a951c66b1de8689dc3a8c9ef517e60
Mati Toledo <toledo.matias@icloud.com>
2022-06-22 19:52:53 -0300

    avatar blog

--- blog/views.py
+++ blog/views.py
@@ -33,8 +33,12 @@ def createBlog(request):
             user = request.user
             image = request.FILES['image']
             user_name = User.objects.get(username=request.user).get_full_name()
-            user_avatar = Avatar.objects.filter(user=request.user.id)
-            blog = Blog(title=info['title'],subtitle=info['subtitle'],body=info['body'],image=image,date=date,user=user,user_name=user_name,user_avatar=user_avatar[0].image.url)
+            avatar = Avatar.objects.filter(user=request.user.id)
+            if len(avatar) == 0:
+              user_avatar = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E"
+            else:
+              user_avatar = avatar[0].image.url
+            blog = Blog(title=info['title'],subtitle=info['subtitle'],body=info['body'],image=image,date=date,user=user,user_name=user_name,user_avatar=user_avatar)
             blog.save()
             return redirect("home:Index")
     form = BlogForm()
--- desafio_python/settings.py
+++ desafio_python/settings.py
@@ -155,7 +155,7 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Heroku configs
