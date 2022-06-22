667eccd5ec4fefa5932dbc387554b8174ccfa797
Mati Toledo <toledo.matias@icloud.com>
2022-06-22 20:02:33 -0300

    featured blogs

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from blog.models import Blog
+
 # Create your views here.
 def index(request):
-  return render(request, 'home/index.html', )
+  featuredBlogs = Blog.objects.all()[1:4].values()
+  return render(request, 'home/index.html',{"featuredBlogs": featuredBlogs} )
