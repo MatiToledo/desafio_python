5ce995a0983e4cc4e55dae899e936423a485e075
Mati Toledo <toledo.matias@icloud.com>
2022-06-25 15:26:21 -0300

    version 1.2

--- blog/urls.py
+++ blog/urls.py
@@ -4,11 +4,11 @@ from blog import views
 
 app_name='blog'
 urlpatterns = [
-   path('', views.index, name='Index'),
+   path('', views.allBlogs, name='Index'),
    path('my-blogs', views.myBlogs, name='MyBlogs'),
    path('create', views.createBlog, name='Create'),
    path('<int:blogId>', views.detailBlog, name='Detail'),
    path('edit/<int:blogId>', views.editBlog, name='Edit'),
    path('delete/<int:blogId>', views.deleteBlog, name="Delete"),
-   path('all', views.allBlogs, name='All'),
+   # path('all', views.allBlogs, name='All'),
 ]
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,22 @@
+import unittest
 from django.test import TestCase
+import unittest
+import random
+import string
+from user.forms import AvatarForm, UserEditForm, UserRegisterForm
+
 
 # Create your tests here.
+class UserTestCase(unittest.TestCase):
+  def setUp(self):
+    pass
+
+  def createUser(self):
+    userLen = 20
+    keyListUser = [random.choice((string.ascii_letters + string.digits)) for i in range(userLen)] 
+    passwordLen = 20
+    keyListPassword = [random.choice((string.ascii_letters + string.digits)) for i in range(passwordLen)] 
+    
+    username = "".join(keyListUser)
+    password = "".join(keyListPassword)
+    UserRegisterForm(username=username,password=password)
